c364524a38c231ea9606e085466cad48ee577a55
Aarif-36 <mohammedaarif823@gmail.com>
2025-11-13 13:02:14 +0530

    Updated homepage design

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/HashWeddings">
       <App />
